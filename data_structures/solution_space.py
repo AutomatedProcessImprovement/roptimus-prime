3e40feabbad18b158fbe577a86d2e31fdadfce2b
Jannis Rosenbaum <j.ro@dock51.de>
2024-03-21 18:46:27 +0200

    fix json

--- data_structures/solution_space.py
+++ data_structures/solution_space.py
@@ -90,7 +90,7 @@ class SolutionOutputObject:
         
 
     def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+        return self.__dict__
     
     
 
