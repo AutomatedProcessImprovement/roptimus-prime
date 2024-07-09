c0c82ab0e62f74861764b58e4361f5d455297a07
Jannis Rosenbaum <j.ro@dock51.de>
2024-07-09 17:54:15 +0300

    fix

--- support_modules/json_manager.py
+++ support_modules/json_manager.py
@@ -21,13 +21,12 @@ class JsonManager:
 
         time_stamp = str(time.time())
         
-        self.base_path_folders = os.path.abspath(os.path.join(SOLUTIONS_FOLDER, time_stamp))
-        self.path = os.path.abspath(os.path.join(self.base_path_folders,'/ids.txt'))
+        self.base_path_folders = os.path.abspath(os.path.join(SOLUTIONS_FOLDER))
+        self.path = os.path.abspath(os.path.join(self.base_path_folders,f'/ids-{time_stamp}.txt'))
         if not os.path.exists(self.path):
             with open(self.path, "w") as f:
                 f.write("")
-        # self.path = "./json_files/ids.txt"
-        # self.base_path_folders = "./json_files/"
+        
 
     def read_file_with_ids(self):
         new_path = self.path
